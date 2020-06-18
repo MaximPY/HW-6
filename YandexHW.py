0bcb70d37e56a76782793e3aba836a6dd1a8ec23
Maxim Pyanov <pyanovmaxim95@gmail.com>
2020-06-18 19:55:08 +0200

    YandexHW modified

--- YandexHW.py
+++ YandexHW.py
@@ -1,36 +1,24 @@
 import requests
 
-def translate_text(num):
+def translate_text(fromfile, tofile, fromlang, tolang='ru'):
     url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
     api_key = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
     list = []
     new_list = []
-    if num == 'DE':
-        name = 'DE.txt'
-        name_1 = 'de_1.txt'
-        lang = 'de-ru'
-    elif num == 'FR':
-        name = 'FR.txt'
-        name_1 = 'fr_1.txt'
-        lang = 'fr-ru'
-    elif num == 'ES':
-        name = 'ES.txt'
-        name_1 = 'es_1.txt'
-        lang = 'es-ru'
-    with open(name, 'r', encoding= 'utf-8') as fr:
+    with open(fromfile, 'r', encoding= 'utf-8') as fr:
         for line in fr:
             line = line.strip()
             list.append(line)
     params = {
         'key': api_key,
         'text': list,
-        'lang': lang
+        'lang': f'{fromlang}-{tolang}'
     }
     response = requests.post(url, params= params)
     new_list.append(response.json()['text'])
-    with open(name_1, 'w', encoding= 'utf-8') as fw:
+    with open(tofile, 'w', encoding= 'utf-8') as fw:
         for data in new_list:
             for elements in data:
                 fw.write(elements)
 
-translate_text('ES')
+translate_text('DE.txt', 'en_1.txt', 'de','en')
